4c271dd22fa6b8697a1807f35a652491aa68479b
shouston3 <shouston3@me.com>
2016-06-18 12:49:20 +0100

    adding mailgun to the server file

--- server.js
+++ server.js
@@ -1,31 +1,45 @@
 const Hapi = require('hapi');
+const Mailgun = require('mailgun').Mailgun;
 require('env2')('./config.env');
+
+const mg = new Mailgun(process.env.MAILGUN);
+const emailAddress = process.env.ADDRESS;
 
 const port = process.env.PORT || 8000;
 
-// Create a server with a host and port
 const server = new Hapi.Server();
 
 server.connection({ port });
 
 server.route([
   {
-    method: 'post',
-    path: '/mail',
+    method: 'get',
+    path: '/mail/{info}',
     handler: function (request, reply) {
-      reply(true);
+      const info = JSON.parse(request.params.info);
+      mg.sendText(
+        info.email,
+        emailAddress,
+        'message from: ' + info.name,
+        'My budget is: ' + info.budget + '\n' +
+        'My website is: ' + info.website + '\n' +
+        'Message: ' + info.message,
+        error => {
+          if(error) reply(false);
+          else reply(true);
+        }
+      );
     }
   },
   {
     method: 'get',
     path: '/',
     handler: function (request, reply) {
-      reply('The factree api, set up to recieve post requests from the /mail endpoint');
+      reply('The factree api, set up to recieve post requests to the /mail endpoint');
     }
   }
 ]);
 
-// Start the server
 server.start(err => {
   if (err) throw err;
   console.log(`server running on: ${server.info.uri}`);
