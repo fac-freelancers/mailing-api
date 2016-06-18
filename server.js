df15d5172b149a2fb7f187fde66c4edc90980c27
shouston3 <shouston3@me.com>
2016-06-18 22:39:08 +0100

    using mailgun-js instead of mailgun as recommended by mailgun and improving email layout

--- server.js
+++ server.js
@@ -1,35 +1,45 @@
 const Hapi = require('hapi');
-const Mailgun = require('mailgun').Mailgun;
 require('env2')('./config.env');
-
-const mg = new Mailgun(process.env.MAILGUN);
-const emailAddress = process.env.ADDRESS;
+const api_key = process.env.MAILGUN;
+const domain = process.env.ADDRESS;
+const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 const port = process.env.PORT || 8000;
 
 const server = new Hapi.Server();
 
-server.connection({ port });
+server.connection({
+  port,
+  routes: {
+    cors: true
+  }
+});
 
 server.route([
   {
     method: 'get',
     path: '/mail/{info}',
     handler: function (request, reply) {
-      if(request.params.info.substring(0, 1) === 't') reply('working');
       const info = JSON.parse(request.params.info);
-      mg.sendText(
-        info.email,
-        emailAddress,
-        'message from: ' + info.name,
-        'My budget is: ' + info.budget + '\n' +
-        'My website is: ' + info.website + '\n' +
-        'Message: ' + info.message,
-        error => {
-          if(error) reply(false);
-          else reply(true);
-        }
-      );
+
+      console.log(info);
+      console.log(api_key, domain);
+
+      const data = {
+        from: info.name + ' <' + info.email + '>',
+        to: 'sam@foundersandcoders.com',
+        subject: 'Message from: ' + info.name,
+        text: 'Website: ' + info.website + '\n' +
+          'Budget: ' + info.budget + '\n' +
+          'Message: ' + info.message
+      };
+
+      mailgun.messages().send(data, function (error, body) {
+        if(error) console.log(error);
+        console.log(data.text);
+        console.log(body);
+        reply('Success');
+      });
     }
   },
   {
